4018a8f33456b6aab7fde1e48b07a2360b49e43a
Business Consult <Business Consult@10.135.113.246>
2019-11-11 20:14:38 -0600

    update

--- WebDevClub2019/WebContent/js/scripts.js
+++ WebDevClub2019/WebContent/js/scripts.js
@@ -1,3 +1,22 @@
-/**
- * 
- */
+window.onload = function () {
+
+		document.getElementsByClassName("mobilemenutrigger")[0].addEventListener("click", mobileMenuFunc);
+		document.getElementsByClassName("exit")[0].addEventListener("click", closeFunc);
+		document.getElementById("mobilemenu").addEventListener("click", closeFunc);
+		document.getElementById("hero").className = "fadeinClass";
+
+		var links = document.querySelectorAll("nav a");
+
+		for(let i = 0; i < links.length; i++)
+		{
+			links[i].addEventListener("click", function(event)
+					{
+				return function()
+				{
+					jsScroll(event);
+					};
+				}(event));
+	}
+}
+
+
